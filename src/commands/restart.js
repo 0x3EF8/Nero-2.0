ad296354fa9e6adf7452303445f2c9cbf1ae6fd4
Jay Patrick Cano <0x3EF8@gmail.com>
2024-10-14 20:21:50 +0800

    Update restart.js

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -1,1 +1,58 @@
+const path = require('path');
+const fs = require('fs');
 
+const packagePath = path.join(__dirname, '..', '..', 'package.json');
+const restartFilePath = path.join(__dirname, '..', '..', 'src', 'data', '@#$&.rs'); 
+
+function getPackageInfo() {
+  try {
+    const packageData = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+    return {
+      name: packageData.name,
+      version: packageData.version,
+    };
+  } catch (error) {
+    return null;
+  }
+}
+
+function restartCommand(event, api) {
+  const input = event.body.toLowerCase().split(' ');
+  const commandName = path.basename(__filename, path.extname(__filename)).toLowerCase();
+  const packageInfo = getPackageInfo();
+
+  if (input.includes('-help')) {
+    const usage = `
+${commandName.charAt(0).toUpperCase() + commandName.slice(1)} Command Help
+
+Command Usage:
+
+1. Basic Command: 
+   - Command: ${commandName}
+   - Description: Restarts the system and logs the restart time.
+   
+2. Help Command:
+   - Command: ${commandName} -help
+   - Description: Displays help information about the command.
+    `;
+    api.sendMessage(usage, event.threadID);
+    return;
+  }
+
+  if (packageInfo) {
+    const restartTime = new Date().toISOString();
+    const logMessage = `${packageInfo.name} ${packageInfo.version} was restarted at: ${restartTime}\n`;
+
+    api.sendMessage(`🔄 Restarting ${packageInfo.name} ${packageInfo.version} in 3 seconds... Please wait.`, event.threadID, event.messageID);
+
+    setTimeout(() => {
+      fs.writeFile(restartFilePath, logMessage, (err) => {
+        if (err) {
+          return;
+        }
+      });
+    }, 3000);
+  }
+}
+
+module.exports = restartCommand;
