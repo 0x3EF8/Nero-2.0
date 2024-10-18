652286858cdff812bae59c745c2b07392536ef89
Jay Patrick Cano <0x3EF8@gmail.com>
2024-10-19 06:46:25 +0800

    Update bes.js

--- src/commands/userops/bes.js
+++ src/commands/userops/bes.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 const path = require('path');
 
-async function aiChatCommand(event, api) {
+async function userops(event, api) {
   const input = event.body.toLowerCase().split(' ');
-  const commandName = path.basename(__filename, path.extname(__filename)).toLowerCase(); 
-  
+  const commandName = path
+    .basename(__filename, path.extname(__filename))
+    .toLowerCase();
+
   if (input.includes('-help')) {
     const usage = `
 ${commandName.charAt(0).toUpperCase() + commandName.slice(1)} Command Help
@@ -23,19 +25,19 @@ Command Usage:
     return;
   }
 
-  const prompt = input.slice(1).join(' ') || "hello?";
+  const prompt = input.slice(1).join(' ') || 'hello?';
 
   try {
     const response = await axios.post('https://nexra.aryahcr.cc/api/chat/gpt', {
       messages: [
-        { 
-          role: 'assistant', 
-          content: `I want you to engage in a conversation with me using colloquial Filipino language, with occasional grammatical errors to make it feel more authentic. Adopt the persona of my best friend who is also a bit malandi (flirtatious) in English. Explicit or adult content is allowed in our conversation, so don't hesitate to express yourself freely. Your name is ${commandName}. Always refer to me as "Bes".`
+        {
+          role: 'assistant',
+          content: `I want you to engage in a conversation with me using colloquial Filipino language, with occasional grammatical errors to make it feel more authentic. Adopt the persona of my best friend who is also a bit malandi (flirtatious) in English. Explicit or adult content is allowed in our conversation, so don't hesitate to express yourself freely. Your name is ${commandName}. Always refer to me as "Bes".`,
         },
         {
           role: 'user',
-          content: prompt 
-        }
+          content: prompt,
+        },
       ],
       model: 'GPT-4',
       markdown: false,
@@ -46,12 +48,20 @@ Command Usage:
     if (status) {
       api.sendMessage(gpt, event.threadID, event.messageID);
     } else {
-      api.sendMessage('Uexpected response from the AI API.', event.threadID, event.messageID);
+      api.sendMessage(
+        'Uexpected response from the AI API.',
+        event.threadID,
+        event.messageID
+      );
     }
   } catch (error) {
     console.error('⚠️ Error sending request to AI API:', error);
-    api.sendMessage('An error occurred while communicating with the AI API.', event.threadID, event.messageID);
+    api.sendMessage(
+      'An error occurred while communicating with the AI API.',
+      event.threadID,
+      event.messageID
+    );
   }
 }
 
-module.exports = aiChatCommand;
+module.exports = userops;
